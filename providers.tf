2307736353b0070573a3dbc23bc8a902cd5b518e
vasu-bhimani-itp <Vasu.Bhimani@intuitive.AI>
2026-04-08 20:03:12 +0530

    instructions are added at fmt level

--- providers.tf
+++ providers.tf
@@ -23,7 +23,7 @@ terraform {
     bucket         = "vasu-task-5-tfstate"
     key            = "vpc/terraform.tfstate"
     region         = "us-east-1"
-    dynamodb_table = " vasu-dynamodb-task-5"
+    dynamodb_table = "vasu-dynamodb-task-5"
     encrypt = true
   }
 }
